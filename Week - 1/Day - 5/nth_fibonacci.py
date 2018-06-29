089835009c894623f0a56c23589e575a98d73e5a
akhild2 <31232879+akhild2@users.noreply.github.com>
2018-06-29 15:48:02 +0530

    Day-5 Complete

---
+++ Week - 1/Day - 5/nth_fibonacci.py
@@ -0,0 +1,54 @@
+import math
+import unittest
+
+a = (1+math.sqrt(5))/2
+b = (1-math.sqrt(5))/2
+
+
+def fib(n):
+    # Compute the nth Fibonacci number
+    if not n<0:
+        return round(((a**2)-(b**2))/math.sqrt(5))
+    raise ValueError
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_zeroth_fibonacci(self):
+        actual = fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        actual = fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        actual = fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        actual = fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        actual = fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        actual = fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2)
